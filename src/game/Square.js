add8d3443017cc8468a4bcf6dcf391d86a316518
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2026-05-09 11:54:08 -0300

    improve board colors

--- src/game/Square.js
+++ src/game/Square.js
@@ -1,11 +1,14 @@
 class Square {
+  static LIGHT = '#f0d9b5';
+  static DARK = '#b58863';
+
   constructor(x, y, row, col, size, color, piece, board) {
     this.x = x;
     this.y = y;
     this.row = row;
     this.col = col;
     this.size = size;
-    this.color = color;
+    this.color = color === 'white' ? Square.LIGHT : Square.DARK;
     this.piece = piece;
     this.board = board;
     this.highlight = false;
@@ -24,8 +27,7 @@ class Square {
     g.rect(this.x, this.y, this.size, this.size, this.color);
 
     if (this.highlight) {
-      // g.rect(this.x, this.y, this.size, this.size, 'rgba(255, 255, 255, 0.5)');
-      g.circle(this.x + this.size / 2, this.y + this.size / 2, this.size / 4, 'red');
+      g.circle(this.x + this.size / 2, this.y + this.size / 2, this.size / 5, 'rgba(0, 0, 0, 0.25)');
     }
 
     if (Game.debug) this.drawDebug(g);
@@ -33,8 +35,7 @@ class Square {
 
   drawDebug(g) {
     if (this.isUnderAttack()) {
-      g.rect(this.x, this.y, this.size, this.size, 'rgba(255, 0, 0, 0.25)');
-      // g.circle(this.x + this.size / 2, this.y + this.size / 2, this.size / 4, 'red');
+      g.rect(this.x, this.y, this.size, this.size, 'rgba(200, 80, 80, 0.2)');
     }
 
     var font = '16px monospace';
--- src/game/pieces/Pawn.js
+++ src/game/pieces/Pawn.js
@@ -18,7 +18,7 @@ class Pawn extends Piece {
   }
 
   calcMoves() {
-    var dir = this.color === Piece.WHITE ? 1 : -1;
+    var dir = this.color === this.board.playerColor ? 1 : -1;
     this.findMovements(dir);
   }
 
@@ -91,7 +91,7 @@ class Pawn extends Piece {
     var fromCol = this.currentSquare.col;
     var isDoubleMove = Math.abs(square.row - fromRow) === 2 && square.col === fromCol;
 
-    var dir = this.color === Piece.WHITE ? 1 : -1;
+    var dir = this.color === this.board.playerColor ? 1 : -1;
     var isPromotion = (dir > 0 && square.row === Board.L8) || (dir < 0 && square.row === Board.L1);
 
     if (isPromotion) {
@@ -135,7 +135,7 @@ class Pawn extends Piece {
     if (!this.board.isEnPassantLegal(this, square)) return;
 
     var fromSquare = this.currentSquare;
-    var dir = this.color === Piece.WHITE ? 1 : -1;
+    var dir = this.color === this.board.playerColor ? 1 : -1;
     var capturedRow = dir > 0 ? square.row + 1 : square.row - 1;
     var capturedSquare = this.board.squares[capturedRow][square.col];
 
